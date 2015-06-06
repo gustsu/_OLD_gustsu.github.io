efc764d317b1554eb84c93fbb76b5ef2cba8e2e2
gustsu <justintew@eagles.ewu.edu>
2015-06-06 13:47:13 -0700

    removed everything, started from scratch

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// My Scripts
+
+//document.write("Hello World!");
+
+$(document).ready(function() {
+  
+  $(".animsition").animsition({
+  
+    inClass               :   'fade-in-right-sm',
+    outClass              :   'fade-out-left-sm',
+    inDuration            :    750,
+    outDuration           :    1000,
+    linkElement           :   '.animsition-link',
+    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+    loading               :    true,
+    loadingParentElement  :   'body', //animsition wrapper element
+    loadingClass          :   'animsition-loading',
+    unSupportCss          : [ 'animation-duration',
+                              '-webkit-animation-duration',
+                              '-o-animation-duration'
+                            ],
+    //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    
+    overlay               :   false,
+    
+    overlayClass          :   'animsition-overlay-slide',
+    overlayParentElement  :   'body'
+  });
+});
